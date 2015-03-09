725caa0f3d9286a4b04695350ed18957a66657b7
rafael.miceli <rafael.miceli@hotmail.com>
2015-03-09 13:38:03 -0300

    DIP implemented

---
+++ src/com/rafaelmiceli/TddWithJava/AzureCloudTable.java
@@ -0,0 +1,11 @@
+package com.rafaelmiceli.TddWithJava;
+
+/**
+ * Created by rafael.miceli on 09/03/2015.
+ */
+public class AzureCloudTable implements CloudTable {
+    @Override
+    public String getById(String id) {
+        return "Id retornado";
+    }
+}
--- src/com/rafaelmiceli/TddWithJava/Main.java
+++ src/com/rafaelmiceli/TddWithJava/Main.java
@@ -4,12 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static CloudTable _cloudTable;
+    private static ArabicToRoman _arabicToRoman;
+
+    public Main(CloudTable cloudTable, ArabicToRoman arabicToRoman) {
+        _cloudTable = cloudTable;
+        _arabicToRoman = arabicToRoman;
+    }
+
     public static void main(String[] args) {
+        //Por que instanciar a classe para executa-la novamente?
+        //Por que a JVM não "constroi" a classe Main,
+        //Por causa disso não é invocado o construtor da mesma.
 
-        ArabicToRoman arabicToRoman = new ArabicToRoman();
+        //Para melhor referencia: http://docs.oracle.com/javase/specs/#41652
 
+        Main main = new Main(new AzureCloudTable(), new ArabicToRoman());
+        
+        main.runProgram();
+    }
+
+    public void runProgram(){
         String keyToExit = "S";
-        String inputKey = "";
+        String inputKey;
         Scanner keyboard = new Scanner(System.in);
 
         do {
@@ -19,24 +36,34 @@ public class Main {
             if (isInteger(inputKey)){
                 Integer inputKeyAsInteger = Integer.parseInt(inputKey);
 
-                String romanNumber = arabicToRoman.convert(inputKeyAsInteger);
+                String romanNumber = _arabicToRoman.convert(inputKeyAsInteger);
 
                 System.out.println(romanNumber);
             }
+            else if (inputKey.equals("D")){
+                readFromCloudTable();
+            }
             else
-                if (!inputKey.equals(keyToExit))
-                    System.out.println("Por favor digite um valor válido");
+            if (!inputKey.equals(keyToExit))
+                System.out.println("Por favor digite um valor válido");
 
 
         } while (!inputKey.equals(keyToExit));
     }
 
-    public static boolean isInteger(String str) {
+
+    public boolean isInteger(String str) {
         try {
             Integer.parseInt(str);
             return true;
         } catch (NumberFormatException nfe) {}
         return false;
+    }
+
+    private void readFromCloudTable() {
+        String result = _cloudTable.getById("id");
+
+        System.out.println(result);
     }
 
 }
